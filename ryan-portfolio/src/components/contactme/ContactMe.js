d6871a9a6e6530934e65315fd7e6bc6c809a444a
Ryan Gleason <ryangleason82@gmail.com>
2019-06-03 20:35:34 -0400

    fin

--- ryan-portfolio/src/components/contactme/ContactMe.js
+++ ryan-portfolio/src/components/contactme/ContactMe.js
@@ -3,13 +3,13 @@ import React from "react";
 const ContactMe = () => {
 	return (
 		<div id="contactme" className="container mt-3">
-			<h3 className="ui horizontal header divider mt-5 mb-5">
+			<h3 className="ui horizontal header divider mt-5">
 				<p>Contact Me</p>
 			</h3>
-			<div className="text-center">
+			<div className="text-center mb-5">
 				<a
 					href="mailto:ryangleason82@gmail.com"
-					className="text-center btn btn-lg btn-dark"
+					className="text-center btn btn-lg btn-dark mb-5"
 				>
 					Email Me!
 				</a>
